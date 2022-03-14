515137e80c88baa5e8e498e36009a37ea2001757
DumDumbIch <gb.dumdumbich@gmail.com>
2022-03-14 13:44:28 +0300

    Add view binding

--- app/src/main/java/com/dumdumbich/proto/handyman/MainActivity.kt
+++ app/src/main/java/com/dumdumbich/proto/handyman/MainActivity.kt
@@ -2,12 +2,17 @@ package com.dumdumbich.proto.handyman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dumdumbich.proto.handyman.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var ui: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ui = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 
