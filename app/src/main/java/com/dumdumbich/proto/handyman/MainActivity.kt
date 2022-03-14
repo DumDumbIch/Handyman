4c302b3fbf639ba12bba417eac64d33ac4611a75
DumDumbIch <gb.dumdumbich@gmail.com>
2022-03-14 14:01:05 +0300

    Add view binding - forgot it)

--- app/src/main/java/com/dumdumbich/proto/handyman/MainActivity.kt
+++ app/src/main/java/com/dumdumbich/proto/handyman/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ui = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(ui.root)
     }
 
 }
