1e48a97f56c8a74b0b445948dcc4d2b4b077519b
DumDumbIch <gb.dumdumbich@gmail.com>
2022-03-17 15:17:26 +0300

    App: DI with Koin

---
+++ app/src/main/java/com/dumdumbich/proto/handyman/App.kt
@@ -0,0 +1,22 @@
+package com.dumdumbich.proto.handyman
+
+import android.app.Application
+import com.dumdumbich.proto.handyman.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@App)
+            modules(appModule)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/dumdumbich/proto/handyman/di/Modules.kt
@@ -0,0 +1,16 @@
+package com.dumdumbich.proto.handyman.di
+
+import com.dumdumbich.proto.handyman.data.net.WeatherDataSource
+import com.dumdumbich.proto.handyman.domain.usecase.WeatherUseCase
+import com.dumdumbich.proto.handyman.ui.pages.home.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+
+val appModule = module {
+
+    single<WeatherUseCase> { WeatherDataSource() }
+
+    viewModel { HomeViewModel(get()) }
+
+}
